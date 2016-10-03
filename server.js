f7f5044ae23cda43c76ec669954591421d1b0154
kiran15061992 <kiran.kiran251@gmail.com>
2016-10-03 23:57:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/goku.png', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'goku.png'));
 });
 
